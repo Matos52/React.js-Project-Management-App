4ae9a848dfa7637bf64c53e5c16203d4ee5df6c7
Matos52 <Matej.Regec@gmail.com>
2024-03-17 23:03:25 +0100

    New components and logic implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const projectId = Math.random();
+
+      const newProject = {
+        ...projectData,
+        id: projectId
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject = {handleStartAddProject} />
+  } else if(projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
     </main>
   );
 }
